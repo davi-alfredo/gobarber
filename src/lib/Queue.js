29f1cfe3f9a664e1479fcd55b04f545bb71be7a7
DavidAlfredo <davi.alfredo@hotmail.com>
2019-11-14 16:03:13 -0300

    Commit continuação do projeto Gobarber

---
+++ src/lib/Queue.js
@@ -0,0 +1,54 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+    this.init();
+  }
+
+  /**
+   * Para cada job é criada auma fila
+   * bee -> carrega as cofigurações do redis para armazenar e recuperar
+   * informações do banco de dados
+   * handle -> é o cara que processa a fila, armazenas as variáveis do contexto
+   */
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig
+        }),
+        handle
+      };
+    });
+  }
+
+  /**
+   * @param {*} queue -> Fila
+   * @param {*} job -> Job que será armazenado na fila
+   */
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  /**
+   * Responsável por recuperar jobs e processá-los
+   */
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.on('failed', this.handleFailure).process(handle);
+    });
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
+  }
+}
+
+export default new Queue();
